7a46526609d63f8e6fd7d8a7e1d15f4dd2e15356
Ereyomi Oluwaseyi <ereyomioluwaseyi@gmail.com>
2020-11-29 20:25:11 +0100

    feat: mean median mode

---
+++ mean-median-mode/mmm.js
@@ -0,0 +1,74 @@
+
+const getMean = ( arrData ) => {
+    let sum = 0
+    for ( let i = 0; i < arrData.length; i++ ) {
+        sum += arrData[ i ]
+    }
+    return sum / arrData.length;
+}
+
+const getMedian = ( arrData ) => {
+
+    let median = 0
+    // sort array
+    const sortArr = arrData.sort( ( a, b ) => a - b )
+
+    if ( sortArr.length % 2 !== 0 ) {
+        // our median will be a mid odd... so
+        median = sortArr[ Math.floor(sortArr.length / 2) ];
+    } else {
+        // [1, 2, 3, 4, 5, 6]
+        let medianA = sortArr[ sortArr.length / 2 ]
+        let medianB = sortArr[ ( sortArr.length / 2 ) - 1 ]
+        median = (medianA + medianB) / 2
+    }
+
+    return median
+}
+
+const getMode = ( arrData ) => {
+    const modeObj = {}
+    for ( let i = 0; i < arrData.length; i++ ) {
+        if ( !modeObj.hasOwnProperty( arrData[ i ] ) ) {
+            modeObj[ arrData[i]] = 0
+        }
+         modeObj[ arrData[i]] +=1 
+    }
+
+    let mode = []
+    let maxFrequency = 0
+    for ( let num in modeObj ) {
+
+        if ( modeObj[ num ] === maxFrequency ) {
+            mode.push(num)
+        }
+
+        if ( modeObj[ num ] > maxFrequency ) {
+            mode.push( num )
+            maxFrequency = modeObj[ num ]
+        }
+        
+
+        
+    }
+    // console.log(maxFrequency,mode,modeObj)
+    // check if the modeArr lenght is equal to the modeObj length
+    if ( mode.length === Object.keys( modeObj ).length ) {
+        mode = []
+    }
+    return mode
+}
+
+const performOperation = ( arr ) => {
+    return {
+        mean: getMean( arr ),
+        median: getMedian( arr ),
+        mode: getMode(arr),
+    }
+};
+
+
+const arrayForOperation = [1, 1, 2, 2, 3, 3]
+
+const operation = performOperation(  arrayForOperation );
+console.log(operation)
