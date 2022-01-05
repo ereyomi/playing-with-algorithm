6dc602c581076a16ad4f07ed011bf87aa90de3ea
Ereyomi Oluwaseyi Samuel <ereyomioluwaseyi@gmail.com>
2022-01-05 23:09:08 +0100

    feat: lcs

---
+++ longest-consecutive-sequence/lcs.js
@@ -0,0 +1,64 @@
+/* 
+
+Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
+
+You must write an algorithm that runs in O(n) time.
+
+ 
+
+Example 1:
+
+Input: nums = [100,4,200,1,3,2]
+Output: 4
+Explanation: The longest consecutive elements sequence is [1, 2, 3, 4]. Therefore its length is 4.
+Example 2:
+
+Input: nums = [0,3,7,2,5,8,4,6,0,1]
+Output: 9
+ 
+
+Constraints:
+
+0 <= nums.length <= 105
+-109 <= nums[i] <= 109
+
+inputs::::
+
+[9,1,4,7,3,-1,0,5,8,-1,6] => 7
+[100,4,200,1,3,2] => 4
+[0,3,7,2,5,8,4,6,0,1] => 9
+[00] => 1
+[0] => 1
+[] => 0
+
+*/
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var longestConsecutive = function(nums) {
+    if(nums.length < 1){
+        return 0;
+    }
+    
+    const as = nums.sort((a,b) => a - b);
+    const s = [...new Set(as)];
+    let i = 0;
+    let hash = []
+    if(s.length === 1 && s[0] === 0){
+        return 1;
+    }
+    let count = 1;
+    while(i <= s.length - 1){
+       
+        if(s[i]+1 === s[i+1]){
+            count++;
+        }
+        else{
+           hash.push(count);
+           count=1;
+        }
+        i++
+    }
+    return Math.max(...hash)
+};
